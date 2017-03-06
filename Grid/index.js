cfc9d375b81c7d669041958097b1ad523174c8d1
Jonathan Werner <jwerner@posteo.de>
2017-03-06 13:19:10 +0100

    add Grid

---
+++ Grid/index.js
@@ -0,0 +1,55 @@
+// @flow
+import React from 'react';
+
+import Flex from '../Flex';
+import Margin from '../Margin';
+
+const nextHigher = {
+  mobile: 'mobileLarge',
+  mobileLarge: 'tablet',
+  tablet: 'desktop',
+  desktop: undefined,
+};
+
+export default function Grid({
+  children,
+  weights,
+  breakAt = 'mobile',
+  spacing = 1,
+}: {
+  breakAt?: 'never' | 'mobile' | 'mobileLarge' | 'tablet' | 'desktop',
+  children?: any,
+  weights?: any,
+  spacing?: any, // 'half', or 0..8
+}): * {
+  const allBreak = (breakAt === 'desktop');
+  const breakAtNext = nextHigher[breakAt] || 'mobile';
+
+  const childCount = React.Children.count(children);
+
+  return (
+    <Flex direction={{mobile: 'column', ...(allBreak ? {} : {[breakAtNext]: 'row'})}}>
+      {React.Children.map(children, (child: *, i: *): * => {
+        if (!child) {
+          return null;
+        }
+
+        const last = i === childCount - 1;
+        return (
+          <Margin
+            right={last ? undefined : {mobile: 0, ...(allBreak ? {} : {[breakAtNext]: spacing})}}
+            bottom={last ? undefined : {mobile: spacing, ...(allBreak ? {} : {[breakAtNext]: 0})}}
+            key={child.key || i}>
+            <Flex.Child
+              auto={{mobile: true, ...(allBreak ? {} : {[breakAtNext]: !!(weights && weights[i] === 0)})}}
+              grow={(weights && weights[i] !== undefined) ? weights[i] : 1}>
+              {child}
+            </Flex.Child>
+          </Margin>
+        );
+      })}
+    </Flex>
+  );
+}
+
+Grid.inheritsClassNames = true;
---
+++ Grid/index.native.js
@@ -0,0 +1,9 @@
+// @flow
+import React from 'react';
+import View from 'mesaic-react-view';
+
+export default function Grid({children}: *): * {
+  return (
+    <View>{children}</View>
+  );
+}
