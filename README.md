4949d6c342673c0d31f4621201d333d48c03281d
Jonathan Glasmeyer <jwerner@posteo.de>
2018-03-07 10:45:24 +0100

    redesign: solve with css variables

--- README.md
+++ README.md
@@ -28,20 +28,31 @@
 ">Foo</div>
 ```
 
+## Contract
+This package expects your consuming environment to be setup in a particular way.
+(1) Variable names passed into `createReplacements` are expected to be available as css vars;
+you should include these in your css somewhere. Example:
+`createReplacements({foregroundColors: ['baseColor', 'secondaryTextColor'], backgroundColors: ['primaryColor', 'secondaryColor']})`
+implies that the following css variable definitions should exist in your consumer CSS:
+```
+:root {
+  /* arbitrary values chosen here */
+  --baseColor: #333;
+  --secondaryTextColor: #777;
+  --primaryColor: blue;
+  --secondaryColor: red;
+}
+```
+
+(2) The following
+
 ## Documentation
-
-For examples and documentation of the components go to http://react-atomic.netlify.com/.
-
-OR
 
 To run that demo on your own computer:
 * Clone this repository
 * `npm install`
 * `npm run storybook`
 * Visit http://localhost:9001/
-
-## Setup
-TODO
 
 [package-url]: https://npmjs.org/package/react-atomic
 [license-image]: http://img.shields.io/npm/l/react-dates.svg
--- utils/createReplacements.js
+++ utils/createReplacements.js
@@ -6,55 +6,20 @@ const _ = require('lodash');
 
 const NUMBERS = /^\d+(\.\d+)$/;
 
-/* eslint-disable no-unused-vars,no-eval,no-console */
-function customEval(code, styles) {
-  try {
-    const fonts = () => (styles.fonts || []).map((font) => {
-      const srcs = [
-        ...(font.woff2 ? [`url('${font.woff2}') format('woff2')`] : []),
-        ...(font.woff ? [`url('${font.woff}') format('woff')`] : []),
-      ];
-
-      return `
-      @font-face {
-        font-family: '${font.familyName}';
-        src: ${srcs.join(',')};
-        font-weight: ${font.weight};
-      }
-      `;
-    }).join('\n');
-    const style = (path, def) => {
-      const result = _.get(styles, path, def);
-      if (typeof result === 'undefined') {
-        throw new Error(`styles.${path} could not be found.`);
-      }
-      return result;
-    };
-    const withUnit = (value, unit) => (typeof value === 'number' || NUMBERS.test(value)) ? `${String(value)}${unit}` : value;
-    const px = (value) => withUnit(value, 'px');
-    const percent = (value) => withUnit(value, '%');
-    const em = (value) => withUnit(value, 'em');
-
-    let __result;
-    eval(`__result = (${code});`);
-    return String(__result);
-  } catch (err) {
-    console.error('error in eval', err);
-    throw err;
-  }
-}
-
-module.exports = (styles) => {
-  if (!styles) {
-    throw new Error('Pass `styles` as first argument.');
-  }
-  if (!styles.colors) {
-    throw new Error('Styles should contain a `colors` key.');
-  }
+// {backgroundColors: string[], foregroundColors: string[]}
+module.exports = ({backgroundColors, foregroundColors}) => {
   return [
     {
-      pattern: /\{\{((.|\n)*?)\}\}/g,
-      replacement: (match, content) => customEval(content, styles),
+      pattern: /__FOREGROUND_COLORS__/,
+      replacement: () => {
+        return foregroundColors.map((colorName) => `.color-${colorName} { color: var(--${colorName}); }`).join('\n')
+      }
+    },
+    {
+      pattern: /__BACKGROUND_COLORS__/,
+      replacement: () => {
+        return backgroundColors.map((colorName) => `.background-color-${colorName} { background-color: var(--${colorName}); }`).join('\n')
+      }
     },
   ];
 };
